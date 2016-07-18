e56265ae29b8801f8936b124b6a42ed4241af038
Sam Stone <sjstone1987@gmail.com>
2016-07-18 13:07:32 -0400

    add STATIC_ROOT

--- platform/settings/base.py
+++ platform/settings/base.py
@@ -83,6 +83,7 @@ STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
